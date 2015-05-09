1c50848e4119b4bd0f577ba8425db929e63b77a6
Paula C Schuler <paulasatx@gmail.com>
2015-05-09 13:38:55 -0500

    Cleaned up calculator.js console.logs.

--- public/JS/calculator.js
+++ public/JS/calculator.js
@@ -1,97 +1,80 @@
-//NOTES ARE AT THE BOTTOM 
+// function () {
+//     "use strict";
 
-var buttons = document.getElementsByClassName("buttons");
-//THE ABOVE LINE RETURNS AN ARRAY!!!!! ayiyiyi
-//it says ELEMENTS PLURAL, hence the array
+// EXERCISE 4.7.3
 
-var spot1Holder = "";
-var spot2Holder = "";
-var operand = "";
-var fieldLeft = document.getElementById("fieldLeft");
-var fieldRight = document.getElementById("fieldRight");
-var result = "";
+    var buttons = document.getElementsByClassName("buttons");     
 
-//THIS ITERATION ADDS A LISTENER TO EVERY BUTTON
-//MAKE IT < BUTTONS.LENGTH.  = IS ASSIGNING. 
-for (var i = 0; i < buttons.length; i++){
-    buttons[i].addEventListener("click", listener, false);
-}
+    var spot1Holder = "";
+    var spot2Holder = "";
+    var operand = "";
+    var fieldLeft = document.getElementById("fieldLeft");
+    var fieldRight = document.getElementById("fieldRight");
+    var result = "";
 
-function listener (event){
-    console.log("listened");
-    console.log(this.value + "above the iffies");
-    var value = this.value; 
-    if (this.value === "c"){
-        clearFields();
+    //THIS ITERATION ADDS A LISTENER TO EVERY BUTTON 
+    for (var i = 0; i < buttons.length; i++)
+    { 
+        buttons[i].addEventListener("click", listener, false);
     }
-    else if (this.value === "="){
-        evaluateFields();
-        clearFieldOperand();
+
+    function listener (event)
+    {
+        var value = this.value; 
+        if (this.value === "c")
+        {
+            clearFields();
+        }
+        else if (this.value === "=")
+        {
+            evaluateFields();
+            clearFieldOperand();
+        }
+        else if (isNaN(this.value))
+        {
+            operand = this.value;
+            document.getElementById("operand").value = operand;
+        }
+        else if (operand != "")
+        {
+            fieldRight.value += this.value;
+        } else 
+        {
+            fieldLeft.value += this.value;
+        }
     }
-    else if (isNaN(this.value)){
-        console.log(this.value + " isn't number");
-        operand = this.value;
-        document.getElementById("operand").value = operand;
+
+    var clearFieldOperand = function ()
+    {
+        document.getElementById("operand").value = "";
+    } 
+
+    var clearFields = function ()
+    {
+        location.reload(true); 
     }
-    else if (operand != ""){
-        console.log(this.value + " this is a number");
-        fieldRight.value += this.value;
-        console.log(fieldRight + " is fieldRight");
-        // document.getElementById("fieldRight").value = fieldRight; 
-    } else {
-        console.log(this.value + " this is for fieldLeft");
-        fieldLeft.value += this.value;
-        console.log(fieldLeft + " is fieldLeft");
+
+    var evaluateFields = function()
+    {
+        operand = document.getElementById("operand");
+        if (operand.value == "+")
+        {
+            fieldLeft.value = parseInt(fieldLeft.value) + parseInt(fieldRight.value);
+            fieldRight.value = "";
+        } else if (operand.value == "-")
+        {
+            fieldLeft.value = parseInt(fieldLeft.value) - parseInt(fieldRight.value);
+            fieldRight.value = "";
+        } else if (operand.value == "*")
+        {
+            fieldLeft.value = parseInt(fieldLeft.value) * parseInt(fieldRight.value);
+            fieldRight.value = "";
+        } else if (operand.value == "/")
+        {
+            fieldLeft.value = parseInt(fieldLeft.value) / parseInt(fieldRight.value);
+            fieldRight.value = "";
+        }
     }
-}
-
-var clearFieldOperand = function (){
-    document.getElementById("operand").value = "";
-} 
-
-var clearFields = function (){
-    location.reload(true); 
-}
-
-var evaluateFields = function(){
-    console.log("found evaluateFields");
-    console.log(fieldLeft.value + " is fieldLeft value");
-    console.log(fieldRight.value + " is fieldRight value");
-    operand = document.getElementById("operand");
-    console.log(operand.value + " is operand value");
-    if (operand.value == "+"){
-        fieldLeft.value = parseInt(fieldLeft.value) + parseInt(fieldRight.value);
-        fieldRight.value = "";
-    } else if (operand.value == "-"){
-        fieldLeft.value = parseInt(fieldLeft.value) - parseInt(fieldRight.value);
-        fieldRight.value = "";
-    } else if (operand.value == "*"){
-        fieldLeft.value = parseInt(fieldLeft.value) * parseInt(fieldRight.value);
-        fieldRight.value = "";
-    } else if (operand.value == "/"){
-        fieldLeft.value = parseInt(fieldLeft.value) / parseInt(fieldRight.value);
-        fieldRight.value = "";
-    }
-}
-    
-
-// DONE---TODO: MAKE OPERATORS VALUE PASS TO FIELDS 
-// create action to happen after click on operands
-// create button variable
-// create listener action call
-
-// TODO: MAKE CLEAR BUTTON DO ITS JOB
-// create action on clear click
-
-// DONE---TODO: 
-// create action to happen after click on numbers
-// create button variables
-// create listener call
-
-// DONE---TODO: GET NUMBER BUTTONS IN PROPER FIELDS 1 OR 3
-
-// DONE: EQUAL BUTTON CALCULATES THE FIELDS AND RETURNS TO FIELD 1
-// create action on equal click--
- 
+// }
 
 
--- public/my_first_form.php
+++ public/my_first_form.php
@@ -3,12 +3,17 @@
   var_dump($_POST);
 ?>
 <html>
+
+<!-- EXERCISE 1.4.1 -->
+<!-- EXERCISE 1.4.2 -->
+<!-- EXERCISE 1.4.4 NOT DONE-->
+
 <head>
 	<title>My First HTML Form</title>
 </head>
 	<body>
 		<h2>User Login</h2>
-		<form method="POST" action="/process-form.php">
+		<form method="POST">
 		    <p>
 		        <label for="username">Username</label>
 		        <input id="username" name="username" type="text" placeholder="Enter your Username">
@@ -22,7 +27,7 @@
 		    </p>
 		</form>
 		<h2>Compose an Email</h2>
-		<form method="POST" action="/process-form.php">
+		<form method="POST">
 		    <p>
 		        <label for="recipient">To: </label>
 		        <input id="recipient" name="recipient" type="text" placeholder="Enter Recipient's Email">
@@ -39,10 +44,61 @@
 		        <label for="message">Username</label>
 		        <input id="message" name="message" type="textarea" placeholder="Write your Message">
 		    </p>
+		    <label for="sendCopy">Do you want to send a copy to your Sent folder?</label>
+		    <input type="checkbox" value="yes" checked>
 		    <p>
 		    	<label for="send">Send</label>
-		        <input type="submit" id="send">
+		        <!-- <input type="submit" id="send" name="send" value="login"> -->
+		        <button type="submit" id="send" name="send" value="login"></button>
 		    </p>
+		</form>
+
+		<form>
+			<p>Multiple Choice Test: Which city is your choice to visit?<br>
+			<label for="Seattle" class="visit">Seattle</label>
+			<input type="checkbox" name="Seattle">
+			<label for="New_York_City" class="visit">New York City</label>
+			<input type="checkbox" name="New_York_City">
+			<label for="London" class="visit">London</label>
+			<input type="checkbox" name="London">
+
+			</p>
+			<p>Where would you want to live?<br>
+			<label for="sanAntonio" class="home">San Antonio</label>
+			<input type="checkbox" name="sanAntonio">
+			<label for="bigSandy" class="home">Big Sandy</label>
+			<input type="checkbox" name="bigSandy">
+			<label for="everett" class="home">San Antonio</label>
+			<input type="checkbox" name="everett">
+			<br>
+			<input type="submit">
+			</p>
+
+		<form>
+			<p>Which are your lodging preferences?<br>
+			<label for="petsAllowed" class="visit">Pets Allowed</label>
+			<input type="radio" name="petsAllowed">
+			<label for="kitchenette" class="preferences">Kitchenette</label>
+			<input type="radio" name="kitchenette">
+			<label for="handicap" class="preferences">Handicap Access</label>
+			<input type="radio" name="handicap">
+			<label for="downtown" class="preferences">Downtown</label>
+			<input type="radio" name="downtown">
+			<label for="airport" class="preferences">Airport</label>
+			<input type="radio" name="airport">
+			<label for="exercise" class="preferences">Exercise Room</label>
+			<input type="radio" name="exercise">
+			<br>
+			<input type="submit">
+			</p>
+		</form>
+			<p>Select Testing NOT WORKING ON SUBMIT. OTHER SUBMIT BUTTONS WORK
+			<label for="testing1">Check box for Yes</label>
+			<input type="checkbox" name="testing1" value="1" checked>
+			<br>
+			<!-- <input type="submit"> -->
+			
+			</p>
 		</form>
 	</body>
 </html>
