6d80c340a0613f7f78fb0b6574c89677e1f3906b
Paula C Schuler <paulasatx@gmail.com>
2015-05-18 19:35:24 -0500

    Cleaned up code for final, this exercise.

--- public/JS/jquery_faq.js
+++ public/JS/jquery_faq.js
@@ -1,24 +1,18 @@
-// $('document').ready(function() {
-//     // $('dd').css("display", "none");
-//     $('#linkToggle').click(function(event) {
-//         event.preventDefault();
-//         $('dd').toggleClass('invisible');
-//     });
-// });
+$(document).ready(function() {
+    $('#linkToggle').click(function(event) {
+        event.preventDefault();
+        $('dd').toggleClass('invisible');
+    });
 
-
-$('ul').each(function(index, element) {
-	// console.log(this);
-	$(this).children().first().css("font-weight", "bold");
-	// console.log($('ul').children().first());
-});
-
-$('document').ready(function() {
+    $('ul').each(function(index, element) {
+		$(this).children().first().css("font-weight", "bold");
+	});
 
     $('li').click(function(event) {
-        event.preventDefault();
-
-        $('ul').addClass('invisible');
+        $(this).parent().addClass('invisible');
     });
 });
+
+
+
 
