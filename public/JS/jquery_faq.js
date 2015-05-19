68785159c70b00a84f3efe9734de4c52bca7a0af
Paula C Schuler <paulasatx@gmail.com>
2015-05-18 19:20:11 -0500

    Finished exercise using jQuery to .addClass on click event.

--- public/JS/jquery_faq.js
+++ public/JS/jquery_faq.js
@@ -1,7 +1,24 @@
+// $('document').ready(function() {
+//     // $('dd').css("display", "none");
+//     $('#linkToggle').click(function(event) {
+//         event.preventDefault();
+//         $('dd').toggleClass('invisible');
+//     });
+// });
+
+
+$('ul').each(function(index, element) {
+	// console.log(this);
+	$(this).children().first().css("font-weight", "bold");
+	// console.log($('ul').children().first());
+});
+
 $('document').ready(function() {
-    // $('dd').css("display", "none");
-    $('#linkToggle').click(function(event) {
+
+    $('li').click(function(event) {
         event.preventDefault();
-        $('dd').toggleClass('invisible');
+
+        $('ul').addClass('invisible');
     });
 });
+
