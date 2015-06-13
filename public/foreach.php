c61e3d188231bc9c73061b946332a7cf471432bb
Paula C Schuler <paulasatx@gmail.com>
2015-06-13 15:45:12 -0500

    PHP foreach loop, exercise 7.5, part two. Complete.

--- public/foreach.php
+++ public/foreach.php
@@ -17,26 +17,28 @@
 			(string) 11
 		);
 
-	foreach($things as $thing) {
-		if (is_int($thing)) {
-			echo "$thing is an integer" . PHP_EOL;
-		} else if (is_float($thing)) {
-			echo "$thing is a float" . PHP_EOL;
-		} else if (is_bool($thing)) {
-			echo "$thing is a boolean" . PHP_EOL;
-		} else if (is_array($thing)) {
-			echo "$thing is an array" . PHP_EOL;
-		} else if (is_null($thing)) {
-			echo "$thing is null" . PHP_EOL;
-		} else if (is_string($thing)) {
-			echo "$thing is a string" . PHP_EOL;
-		}
-	}
-
+	// identifies data type of array values
 	// foreach($things as $thing) {
-	// 	if (is_scalar($thing)) {
-	// 		echo $thing is scalar" . PHP_EOL;
+	// 	if (is_int($thing)) {
+	// 		echo "$thing is an integer" . PHP_EOL;
+	// 	} else if (is_float($thing)) {
+	// 		echo "$thing is a float" . PHP_EOL;
+	// 	} else if (is_bool($thing)) {
+	// 		echo "$thing is a boolean" . PHP_EOL;
+	// 	} else if (is_array($thing)) {
+	// 		echo "$thing is an array" . PHP_EOL;
+	// 	} else if (is_null($thing)) {
+	// 		echo "$thing is null" . PHP_EOL;
+	// 	} else if (is_string($thing)) {
+	// 		echo "$thing is a string" . PHP_EOL;
 	// 	}
 	// }
+
+	// identifies scalar datatype items in array
+	foreach($things as $thing) {
+		if (is_scalar($thing)) {
+			echo "$thing is scalar" . PHP_EOL;
+		}
+	}
 
  ?>
