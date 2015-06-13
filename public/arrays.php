4ad7d1427385ffab2dca4eefb1ef84cfaa3ba561
Paula C Schuler <paulasatx@gmail.com>
2015-06-13 14:56:37 -0500

    cleaned up code.

--- public/arrays.php
+++ public/arrays.php
@@ -1,5 +1,5 @@
 <?php 
-// EXERCISE 7.3 
+// EXERCISE 7.3 COMPLETE
 
 	$numbers = array(1, 2, 3, 4, 5);
 	var_dump($numbers) . PHP_EOL;
@@ -11,31 +11,31 @@
 	echo $numbers[3] . PHP_EOL;
 
 	$contact = [
-		"firstName" => "Paula",
-		"lastName" => "Schuler",
-		"email" => "paula@gmail.com",
-		"phone" => "210-555-5555"
+		"firstName" 	=> "Paula",
+		"lastName" 		=> "Schuler",
+		"email" 		=> "paula@gmail.com",
+		"phone" 		=> "210-555-5555"
 	];
 	var_dump($contact);
 
 	$test = [
 		"one" => [
-			"firstName" => "Paula",
-			"lastName" => "Schuler",
-			"email" => "paula@gmail.com",
-			"phone" => "210-555-5555"
+			"firstName" 	=> "Paula",
+			"lastName" 		=> "Schuler",
+			"email" 		=> "paula@gmail.com",
+			"phone" 		=> "210-555-5555"
 		],
 		"two" => [
-			"firstName" => "Carol",
-			"lastName" => "Burnett",
-			"email" => "carol@gmail.com",
-			"phone" => "210-555-3333"
+			"firstName" 	=> "Carol",
+			"lastName" 		=> "Burnett",
+			"email" 		=> "carol@gmail.com",
+			"phone" 		=> "210-555-3333"
 		],
 		"three" => [
-			"firstName" => "Barack",
-			"lastName" => "Obama",
-			"email" => "barack@gmail.com",
-			"phone" => "210-111-1111"
+			"firstName" 	=> "Barack",
+			"lastName" 		=> "Obama",
+			"email" 		=> "barack@gmail.com",
+			"phone" 		=> "210-111-1111"
 		]
 	];
 	var_dump($test);
