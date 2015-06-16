d01982ad1719331f019385b79beb76810b6eb416
Paula C Schuler <paulasatx@gmail.com>
2015-06-15 19:48:33 -0500

    Exercise 7.7 PHP function scope and arithmetic, complete.

--- public/arithmetic.php
+++ public/arithmetic.php
@@ -1,4 +1,10 @@
 <?php
+// EXERCISE 7.7 PHP ARITHMETIC - a study in scope - COMPLETE
+
+// These variables have no impact on returns 
+// because they were not passed in the function calls at the bottom
+$a = 12;
+$b = 6;
 
 function add($a, $b)
 {
@@ -20,10 +26,19 @@ function divide($a, $b)
     return $a / $b;
 }
 
-$a = 6;
-$b = 9;
+function modulus($a, $b)
+{
+	return $a % $b;
+}
+
+function test($a, $b)
+{
+	return $a / $b;
+}
+
 echo add(6, 9) . " added" . PHP_EOL;
 echo subtract(6, 9) . " subtracted" . PHP_EOL;
 echo multiply(6, 9) . " multiplied" . PHP_EOL;
 echo divide(6, 9) . " divided" . PHP_EOL;
-
+echo modulus(6, 9) . " modulus" . PHP_EOL;
+echo test(6, 9) . " testing modulus" . PHP_EOL;
