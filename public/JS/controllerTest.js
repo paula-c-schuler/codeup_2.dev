8a3d6084bbc256e7014d0f220c36cce403d325b5
Paula C Schuler <paulasatx@gmail.com>
2015-06-27 15:39:35 -0500

    CSS progress in styling shopping cart user input and display.

--- public/JS/controllerTest.js
+++ public/JS/controllerTest.js
@@ -3,5 +3,17 @@
 (function() {                                  
     var app = angular.module("controllerTest", []);
     
-    
+    app.controller("CartController", function() {
+
+    	this.items = [];
+    	this.newItem = {
+    		"quantity": 1
+    	};
+
+    	this.items.push(this.newItem);
+    	this.newItem = {
+    		"quantity": 1
+    	};
+
+    });
 })();
