57f4d32d4e5501834bd083c255a6209a7ac14116
Paula C Schuler <paulasatx@gmail.com>
2015-06-29 18:23:00 -0500

    Finished basic assignment of ex. 9.2 Angular. Want to improve styling.

--- public/JS/modelTest.js
+++ public/JS/modelTest.js
@@ -1,5 +1,24 @@
+// Exercise 9.2 Angular JS models
+// This file is complete per assignment instructions
+// WANT TODO: Write my own filter. Below is a starter found on github, but buggy
+
 "use strict";
 
 (function() {                                  
     var app = angular.module("modelTest", []);
+
+    // a filter for capitalizing first letter of words in string
+    // Capitalize filter has a bug? 
+    	// app.filter('capitalize', function() {
+    	// 	if (input!=null)
+    	// 	input = input.toLowerCase();
+    	// 	return input.substring(0,1).toUpperCase()+input.substring(1);
+    	// });
+
+    // app is the models
+
+    app.controller("ValidationFeedback", function() {
+    	// code goes here 
+    })
+
 })();
