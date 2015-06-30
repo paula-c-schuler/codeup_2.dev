4cbd465aca4077d42c86fb5cc8ab79399dca9916
Paula C Schuler <paulasatx@gmail.com>
2015-06-29 21:02:32 -0500

    Success storing user inputs onto array, displaying growing array on shopping cart. Ex. 9.3

--- public/JS/controllerTest.js
+++ public/JS/controllerTest.js
@@ -5,15 +5,22 @@
     
     app.controller("CartController", function() {
 
-    	this.items = [];
-    	this.newItem = {
-    		"quantity": 1
-    	};
+        // initialize items array and newItem JSON
+        this.items = [];
+        this.newItem = {
+            "quantity": 1,
+        };
 
+    // Saves item inputs by user onto items array
+    // and then resets initial array. 
+    this.saveItems=function() {
     	this.items.push(this.newItem);
+        
     	this.newItem = {
     		"quantity": 1
     	};
+        console.log(this.items);
+    }
 
     });
 })();
--- public/JS/template-Angular.js
+++ public/JS/template-Angular.js
@@ -1,3 +1,5 @@
+// NEED TODO: ADD SOME EXAMPLE FUNCTIONS AND ADD DEPENDENCIES
+
 "use strict";
 
 (function() {                                  
@@ -9,4 +11,29 @@
     		return input.substring(0,1).toUpperCase()+input.substring(1);
     	}
     });
+
+	app.controller("ApolloController", )
+		this.users = [];
+
+		this.newUser = {};
+
+		this.saveUser = function(userForm) {
+
+			this.users.push(this.newUser);
+
+			this.newUser = {};
+		}
+
+	// using service -- code not finished
+	app.controller("ApolloController", ["$log", function($log) {
+		this.users = [];
+
+		this.newUser = {};
+
+		this.saveUser = function(userForm) {
+
+			this.users.push(this.newUser);
+
+			this.newUser = {};
+	}])
 })();
