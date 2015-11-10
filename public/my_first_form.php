daf2ac3c356ac7dbd57b9effcd347b675d8b0ad3
Paula C Schuler <paulasatx@gmail.com>
2015-11-09 20:28:35 -0600

    Adjustments due to learning how to nest label and input fields. ETC.

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -10,8 +10,29 @@
 
 <head>
 	<title>My First HTML Form</title>
+	<style type="text/css">
+		body	{
+			margin-left: 5%;
+			font-size: 1.5rem;
+		}
+		input {
+			font-size: 1.25rem;
+		}
+		button {
+			padding: 4px;
+			background-color: #2e26666;
+			color: black;
+			font-size: 1.25rem;
+		}
+		select {
+			/*font-size: 1.25;*/
+			/*padding: 5px;*/
+			height: 10px;
+		}
+	</style>
 </head>
 	<body>
+		<!-- USER LOGIN FORM  -->
 		<h2>User Login</h2>
 		<form method="POST">
 		    <p>
@@ -23,83 +44,128 @@
 		        <input id="password" name="password" type="password" placeholder="Enter your Password">
 		    </p>
 		    <p>
-		        <input type="submit">
+		        <button type="submit">Submit</button>
 		    </p>
 		</form>
+		<br>
+		<!-- COMPOSE AN EMAIL FORM  -->
 		<h2>Compose an Email</h2>
 		<form method="POST">
 		    <p>
-		        <label for="recipient">To: </label>
-		        <input id="recipient" name="recipient" type="text" placeholder="Enter Recipient's Email">
+		        <label for="recipient">TO: </label>
+		        <input id="recipient" name="recipient" type="text">
 		    </p>
 		    <p>
-		        <label for="sender">From: </label>
-		        <input id="sender" name="sender" type="text" placeholder="Enter your Email">
+		        <label for="sender">FROM: </label>
+		        <input id="sender" name="sender" type="text">
 		    </p>
 		    <p>
-		        <label for="subject">Subject: </label>
-		        <input id="subject" name="subject" type="text" placeholder="Enter your Subject">
+		        <label for="subject">SUBJECT: </label>
+		        <input id="subject" name="subject" type="text">
 		    </p>
 		    <p>
-		        <label for="message">Username</label>
+		        <label for="message">YOUR MESSAGE:</label>
 		        <input id="message" name="message" type="textarea" placeholder="Write your Message">
 		    </p>
-		    <label for="sendCopy">Do you want to send a copy to your Sent folder?</label>
-		    <input type="checkbox" value="yes" checked>
-		    <p>
-		    	<label for="send">Send</label>
-		        <!-- <input type="submit" id="send" name="send" value="login"> -->
-		        <button type="submit" id="send" name="send" value="login"></button>
+			    <p>Do you want to send yourself a copy?</p>
+			    <label for="sendCopy">Yes
+			    	<input type="checkbox" value="yes" checked>
+			    </label>
+			    <br>
+		    <p>      
+		        <button type="submit" id="send" name="send" value="login">Send It! </button>
 		    </p>
 		</form>
-
+		<br>
+		<!-- MULTIPLE CHOICE TEST first way I learned -->
 		<form>
-			<p>Multiple Choice Test: Which city is your choice to visit?<br>
-			<label for="Seattle" class="visit">Seattle</label>
-			<input type="checkbox" name="Seattle">
-			<label for="New_York_City" class="visit">New York City</label>
-			<input type="checkbox" name="New_York_City">
-			<label for="London" class="visit">London</label>
-			<input type="checkbox" name="London">
+			<h2>Multiple Choice Test:</h2>
+			<p><h3>Which city is your choice to visit?</h3></p>
+			<label for="Seattle">Polcinego</label>
+			<input type="checkbox" value="Seattle" name="visitArray">
+			<label for="New_York_City">New York City</label>
+			<input type="checkbox" value="New York City" name="visitArray">
+			<label for="London">London</label>
+			<input type="checkbox" value ="London" name="visitArray">
+			<button type="submit">Submit</button>
+			<br>
+			<br>
+			<!-- Using cityArray[] for creating an array -->
+			<p><h3>What is your cityArray choice?</h3></p>
+			<label>Seattle
+				<input type="checkbox" value="Seattle" name="cityArray[]">
+			</label>
+			<label>New York City
+				<input type="checkbox" value="New York City" name="cityArray[]">
+			</label>
+			<label>London
+				<input type="checkbox" value="London" name="cityArray[]">
+			</label>
+			<button type="submit">Submit</button>
+			<br>
+			<br>
 
-			</p>
-			<p>Where would you want to live?<br>
-			<label for="sanAntonio" class="home">San Antonio</label>
-			<input type="checkbox" name="sanAntonio">
-			<label for="bigSandy" class="home">Big Sandy</label>
-			<input type="checkbox" name="bigSandy">
-			<label for="everett" class="home">San Antonio</label>
-			<input type="checkbox" name="everett">
+			<!-- CHECKBOXES ON FORM -->
+			<p><h3>Where would you want to live?</h3></p>
+			<label>San Antonio
+				<input type="checkbox" for"SanAntonio" class="home" name="homeArray[]">
+			</label>
+			<label>Boston
+				<input type="checkbox" for"Boston" class="home" name="homeArray[]">
+			</label>
+			<label>Helotes
+				<input type="checkbox" for"Helotes" class="home" name="homeArray[]">
+			</label>
 			<br>
-			<input type="submit">
-			</p>
-		</form>
+			<button type="submit">Submit</button>
+			<br>
+			<br>
 
-		<form>
-			<p>Which are your lodging preferences?<br>
-			<label for="petsAllowed" class="visit">Pets Allowed</label>
-			<input type="radio" name="petsAllowed">
-			<label for="kitchenette" class="preferences">Kitchenette</label>
-			<input type="radio" name="kitchenette">
-			<label for="handicap" class="preferences">Handicap Access</label>
-			<input type="radio" name="handicap">
-			<label for="downtown" class="preferences">Downtown</label>
-			<input type="radio" name="downtown">
-			<label for="airport" class="preferences">Airport</label>
-			<input type="radio" name="airport">
-			<label for="exercise" class="preferences">Exercise Room</label>
-			<input type="radio" name="exercise">
+			<!-- CHECKBOXES ON FORM -->
+			<p><h3>Which are your lodging preferences?</h3></p>
+			<label>Pets Allowed
+				<input type="radio" name="petsAllowed">
+			</label>
+			<label>Kitchenette
+				<input type="radio" name="kitchenette">
+			</label>
+			<label>Handicap Access
+				<input type="radio" name="handicap">
+			</label>
+			<label>Downtown
+				<input type="radio" name="downtown">
+			</label>
+			<label>Airport
+				<input type="radio" name="airport">
+			</label>
 			<br>
-			<input type="submit">
-			</p>
-		</form>
-		<form>
-			<p>Select Testing <br>
-			<label for="testing1">Check box for Yes</label>
-			<input type="checkbox" name="testing1" value="1" checked>
+			<button type="submit">Submit</button>
 			<br>
-			<input type="submit">
-			</p>
-		</form>
+			<br>
+		
+			<!-- TEXTAREA TAG -->
+			<p><h2>Tell Us What You Think</h2></p>
+				<textarea id="comments" name="comments" rows="10" cols="50"></textarea>
+				<button type="submit">Submit</button>
+			<br>
+			<br>
+
+			<!-- SELECT AND OPTION TAGS = Drop Down and Select Option -->
+			<label for="state">State
+				<select id="state" name="state">
+					<option>AL</option>
+					<option>AK</option>
+					<option>CO</option>
+					<option>LA</option>
+					<option>OK</option>
+					<option>TX</option>
+				</select>
+			</label>
+			<br>
+			<br>
+			<button type="submit">Submit</button>
+
+
+		</form>	
 	</body>
 </html>
