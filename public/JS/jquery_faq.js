b6099406792e9ab2c1f76f6f997e3aafc4149078
Paula C Schuler <paulasatx@gmail.com>
2016-01-28 20:24:20 -0600

    jQuery manipulation of the DOM, parents, children, siblings.

--- public/JS/jquery_faq.js
+++ public/JS/jquery_faq.js
@@ -1,11 +1,27 @@
 $(document).ready(function() {
+    "use strict";
 
-    $('#linkToggle').click(function(event) {
-        event.preventDefault();
-        $('#linkToggle').siblings(function(){
+    // $('#linkToggle').click(function(event) {
+    //     event.preventDefault();
+    //     $('#linkToggle').siblings(function(){
         	 
-    	});
+    // 	});
+    // });
+
+    // Make first li under each ul bold
+    $("ul li:first-child")
+        .css({"font-weight": "bold"})
+
+
+    //Make parent of clicked li tag invisible
+    $("li").click(function(){
+        console.log(this);
+        
+        var h3Tag = ($(this).parent().siblings("h3"));
+        console.log(h3Tag);
+        h3Tag.addClass("invisible");
     });
+
 
     $('h3').click(function(event) {
 		console.log(this);
