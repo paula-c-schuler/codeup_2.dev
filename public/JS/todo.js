ed5d9ab5ba2b15829c5b913182db60109e50db03
Paula C Schuler <paulasatx@gmail.com>
2016-02-17 20:38:14 -0600

    removed date field pulls, studied ajax store file, backed up a bit.

--- public/JS/todo.js
+++ public/JS/todo.js
@@ -1,42 +1,48 @@
 $(document).ready(function(){
 console.log("document loaded");
+
 	$('#btn-new-item').click(function(event){
 		event.preventDefault();
+		// assigning extracted data to variables for easy manipulation
 		var content = $("#content").val();
 		var priority = $("#priority").val();
-		var dateListed = $("#due_date").val();
 		var rowString;
-
+		// This variable holds JSON specific values extracted from .click on #btn-new-item
 		var data = {
-			"content":content,
-			"priority": priority
+			"content": content,
+			"priority": priority,
 		};
 		$.post("todo-json.php", data, function(item){
 			console.log(item);
 			console.log(item.content);
+			console.log(item.priority);
+			console.log(data);
+
+			rowString = "";
+			rowString += "<tr>" + 
+					"<td>" +
+						item.content + 
+					"</td>" +
+					"<td>" + 
+						item.priority + 
+					"</td>" +
+					"</tr>";
+			
+			$('#resultsRows').html(rowString);
 		});
 	});
 
 	$('#btn-show-items').click(function(event){
 		event.preventDefault();
-		$.get("todo-json.php", function(items){
-			for(var i = 0; i < items.length; i++){
-			// items.forEach(function(element,index){
-				console.log(items[i].content);
-				console.log(items.length);
-				console.log(items.priority);
-				rowString = "";
-				rowString += "<tr>" + 
-						"<td>" +
-							items[i].content + 
-						"</td>" +
-						"<td>" + 
-							items[i].priority + 
-						"</td>" +
-						"</tr>";
+		$.get("todo-json.php", function(data){
 			
-			$('#resultsRows').html(rowString);
-			}
-		})
+			$.each(data(function(element, index){
+				console.log(element);
+				// console.log(item.content);
+				// console.log(item.length);
+				// console.log(item.priority);
+			
+			}));
+		});
 	});
 });
