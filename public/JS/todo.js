63422db51b4b3bba867d336b0fd35902a2af04b0
Paula C Schuler <paulasatx@gmail.com>
2016-02-17 20:56:12 -0600

    end of the night

--- public/JS/todo.js
+++ public/JS/todo.js
@@ -6,17 +6,20 @@ console.log("document loaded");
 		// assigning extracted data to variables for easy manipulation
 		var content = $("#content").val();
 		var priority = $("#priority").val();
+		var datListed = $("#due_date").val();
 		var rowString;
 		// This variable holds JSON specific values extracted from .click on #btn-new-item
 		var data = {
 			"content": content,
 			"priority": priority,
 		};
+
+		// data and funtion portions are optional 
+		// funtion is optional
 		$.post("todo-json.php", data, function(item){
 			console.log(item);
 			console.log(item.content);
 			console.log(item.priority);
-			console.log(data);
 
 			rowString = "";
 			rowString += "<tr>" + 
@@ -35,14 +38,19 @@ console.log("document loaded");
 	$('#btn-show-items').click(function(event){
 		event.preventDefault();
 		$.get("todo-json.php", function(data){
-			
-			$.each(data(function(element, index){
+			console.log(data);
+			$.each(data, function(index, element){
 				console.log(element);
-				// console.log(item.content);
-				// console.log(item.length);
-				// console.log(item.priority);
+
+			});
+
+			// $.each(data(function(element, index){
+			// 	console.log(element);
+			// 	// console.log(item.content);
+			// 	// console.log(item.length);
+			// 	// console.log(item.priority);
 			
-			}));
+			// }));
 		});
 	});
 });
